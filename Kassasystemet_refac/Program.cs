bd9cea95df006a099a70cc56632a04ac2116a997
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-08-19 14:54:51 +0200

    Refaktorering av kassasystemet BOOM!

---
+++ Kassasystemet_refac/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kassasystemet_refac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
