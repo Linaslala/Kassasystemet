1dfe0a06eefc706f80aab61a182f22858682f579
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-08-20 11:47:26 +0200

    Har flyttat ReadAllMembersFromFile till en egen fil

--- Kassasystemet_refac/Program.cs
+++ Kassasystemet_refac/Program.cs
@@ -3761,32 +3761,7 @@ namespace Kassasystemet_refac
                 }
             }
         }
-        public class ReadAllMembersFromFile : IReadAllMembersFromFile
-        {
-            public List<IMemberModel> ReadAll()
-            {
-                var members = new List<IMemberModel>();
-
-                string filePath = MemberFilePath.MembersPath;
-
-                if (!File.Exists(filePath))
-                    return members;
-
-                var lines = File.ReadAllLines(filePath);
-
-                foreach (var line in lines)
-                {
-                    var parts = line.Split(';');
-                    if (parts.Length != 3) continue;
-
-                    if (int.TryParse(parts[0], out int memberId))
-                    {
-                        members.Add(new MemberModel(memberId, parts[1], parts[2]));
-                    }
-                }
-                return members;
-            }
-        }
+       
         internal static class MemberFilePath
         {
             private static string EnsureTextFilesDir()
