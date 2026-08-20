1121ecfed14bb4d46823053872e9c894b04b0893
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-08-20 11:06:34 +0200

    Flyttat MemberModel till egen fil

---
+++ Kassasystemet_refac/Members/MemberModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Kassasystemet_refac.SearchMember;
+
+namespace Kassasystemet_refac
+{
+    public class MemberModel : IMemberModel
+    {
+        public int MemberIdNumber { get; }
+        public string MemberFirstName { get; }
+        public string MemberLastName { get; }
+        public string MemberFullName => $"{MemberFirstName} {MemberLastName}";
+
+        public MemberModel(int memberIdNumber, string memberFirstName, string memberLastName)
+        {
+            MemberIdNumber = memberIdNumber;
+            MemberFirstName = memberFirstName;
+            MemberLastName = memberLastName;
+        }
+    }
+}
--- Kassasystemet_refac/Program.cs
+++ Kassasystemet_refac/Program.cs
@@ -4030,20 +4030,7 @@ namespace Kassasystemet_refac
                     .ToList();
             }
         }
-        public class MemberModel : IMemberModel
-        {
-            public int MemberIdNumber { get; }
-            public string MemberFirstName { get; }
-            public string MemberLastName { get; }
-            public string MemberFullName => $"{MemberFirstName} {MemberLastName}";
-
-            public MemberModel(int memberIdNumber, string memberFirstName, string memberLastName)
-            {
-                MemberIdNumber = memberIdNumber;
-                MemberFirstName = memberFirstName;
-                MemberLastName = memberLastName;
-            }
-        }
+        
         public interface ISearchMember
         {
             List<IMemberModel> Search(string searchMemberText);
