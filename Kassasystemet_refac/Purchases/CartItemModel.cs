7f2b403ae70508cf3d9124279563b57659f02112
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-08-20 11:11:36 +0200

    Flyttat CartItemModel tillen egen fil

---
+++ Kassasystemet_refac/Purchases/CartItemModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Kassasystemet_refac
+{
+    public class CartItemModel
+    {
+        public int ProductIdNumber { get; }
+        public string ProductName { get; }
+        public decimal ProductPrice { get; }
+        public string PriceType { get; }
+        public int ProductQuantity { get; }
+
+        public CartItemModel(int productIdNumber, string productName, decimal productPrice, string priceType, int productQuantity)
+        {
+            ProductIdNumber = productIdNumber;
+            ProductName = productName ?? "";
+            ProductPrice = productPrice;
+            PriceType = priceType ?? "";
+            ProductQuantity = productQuantity;
+        }
+
+        public decimal LineTotal => ProductPrice * ProductQuantity;
+
+        public CartItemModel WithQuantity(int newQuantity)
+            => new CartItemModel(ProductIdNumber, ProductName, ProductPrice, PriceType, newQuantity);
+    }
+}
