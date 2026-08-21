0c722c226a0c142835d14162b0c24d7e6e154b40
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-08-21 13:11:13 +0200

    Skapat en klass NotificationService för att börja göra min kod mer DRY.

---
+++ Kassasystemet_refac/Utilities/NotificationService.cs
@@ -0,0 +1,21 @@
+﻿namespace Kassasystemet_refac
+{
+    public static class NotificationService
+    {
+        public static void ShowError(
+            string message)
+        {
+            Console.Clear();
+            Console.ForegroundColor =
+                ConsoleColor.Red;
+
+            CenterConsoleOutput
+                .CenterTextToWindow(message);
+
+            Console.ResetColor();
+
+            ValidatedConsoleInput
+                .PauseCentered();
+        }
+    }
+}
